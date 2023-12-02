61484cc85baf7c96aa8f2fb6168a018272457d59
DonDejvo <dada542cz@gmail.com>
2023-12-02 16:18:30 +0100

    home CSS update

--- views/home/index.php
+++ views/home/index.php
@@ -1,25 +1,45 @@
 <span class="brand">Home</span>
 
-<h1 class="title">Game Archive</h1>
+<h1 class="home-title">Game Archive</h1>
 
 <div>
 
-<p class="home-description">Welcome to GameArchive, your interactive hub for HTML5 gaming! Dive into a world of diverse games, explore user-generated content, and enjoy a seamless experience. Your journey into the exciting realm of HTML5 games starts here!</p>
+<p class="home-description">Are you ready to elevate your gaming experience? Look no further than GameArchive, your all-in-one destination for HTML5 gaming. Here's why you should join the excitement</p>
 
+<div class="centered">
 <a class="btn" href="games.php">Check our games</a>
+</div>
 
 </div>
 
 <div class="home-row">
 
 <div class="home-card">
-    <h2 class="home-card__title">Join us now!</h2>
-    <p>Ready to level up your gaming experience? Become a part of the GameArchive community today!</p>
+    <h2 class="home-card__title">Explore a Diverse Gaming Universe</h2>
+    <p>Immerse yourself in a vast collection of HTML5 games, ranging from action-packed adventures to mind-bending puzzles</p>
 </div>
 
 <div class="home-card">
-    <h2 class="home-card__title">Hi, I am David!</h2>
-    <p>I am studying software engineering at Czech Technical University and I am HTML games enthusiasist.</p>
+    <h2 class="home-card__title">Connect with a Vibrant Community</h2>
+    <p>Engage with fellow gamers, share insights, and discover hidden gems. GameArchive is not just a platform; it's a community that celebrates the love for gaming.</p>
 </div>
 
+<div class="home-card">
+    <h2 class="home-card__title">Unleash Your Creativity</h2>
+    <p>Showcase your gaming prowess! Build a personalized profile, curate lists of your favorite games, and let the world know about your gaming journey.</p>
+</div>
+
+<div class="home-card">
+    <h2 class="home-card__title">User-Generated Content</h2>
+    <p>Be part of a platform that thrives on user contributions. Share your thoughts, recommend games, and become a key player in shaping the GameArchive experience.</p>
+</div>
+
+</div>
+
+<h2 class="centered">Ready to Begin Your Journey? Join GameArchive Today!</h2>
+
+<p class="home-description">Don't miss out on the gaming revolution. Join GameArchive now and unlock a world of possibilities. Whether you're a casual gamer or a seasoned pro, there's something for everyone in the vibrant universe of GameArchive. Let the games begin!</p>
+
+<div class="centered">
+<a class="btn" href="register.php">Join us</a>
 </div>
