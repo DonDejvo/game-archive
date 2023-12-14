6226fdfe24a4c434545fa8a30ea5e45253d08b98
DonDejvo <dada542cz@gmail.com>
2023-12-14 20:18:48 +0100

    games filter fix

--- public/js/pagination.js
+++ public/js/pagination.js
@@ -1,27 +1,22 @@
 (function () {
     const paginationLinks = document.querySelectorAll(".pagination__item > a");
-    const searchInput = document.getElementById("search");
-    const genreInput = document.getElementById("genre");
-    const filterInput = document.getElementById("filter");
+    const filterWidget = document.querySelector(".games-filter-widget");
 
     for (let i = 0; i < paginationLinks.length; ++i) {
         const elem = paginationLinks[i];
         elem.addEventListener('click', () => updateQueryParam('page', elem.dataset.page));
     }
 
-    if (paginationLinks.length) {
-        if (searchInput) {
-            const searchBtn = document.getElementById("search-btn");
-            searchBtn.addEventListener('click', () => updateQueryParam('search', searchInput.value));
-        }
+    if (filterWidget) {
 
-        if (genreInput) {
-            genreInput.addEventListener('change', () => updateQueryParam('genre', genreInput.value));
-        }
+        const searchInput = filterWidget.getElementById("search");
+        const genreInput = filterWidget.getElementById("genre");
+        const filterInput = filterWidget.getElementById("filter");
+        const searchBtn = filterWidget.getElementById("search-btn");
 
-        if (filterInput) {
-            filterInput.addEventListener('change', () => updateQueryParam('filter', filterInput.value));
-        }
+        searchBtn.addEventListener('click', () => updateQueryParam('search', searchInput.value));
+        genreInput.addEventListener('change', () => updateQueryParam('genre', genreInput.value));
+        filterInput.addEventListener('change', () => updateQueryParam('filter', filterInput.value));
     }
 
     function updateQueryParam(name, value) {
